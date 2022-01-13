f3e1208d8b666a482c18529a471d1ccd828d8cb4
dingding-21 <phj8221@naver.com>
2022-01-14 03:46:49 +0900

    [FEAT] user login

--- functions/api/routes/auth/authLoginPOST.js
+++ functions/api/routes/auth/authLoginPOST.js
@@ -1,24 +1,21 @@
+const _ = require('lodash');
 const functions = require('firebase-functions');
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const { signInWithEmailAndPassword } = require('firebase/auth');
 const db = require('../../../db/db');
-const { userDB, typeDB, projectDB } = require('../../../db');
+const { userDB, projectDB, typeDB, tagDB, positionDB, fieldDB } = require('../../../db');
 
 const { firebaseAuth } = require('../../../config/firebaseClient');
 
 const jwtHandlers = require('../../../lib/jwtHandlers');
-const { user } = require('firebase-functions/v1/auth');
 
 module.exports = async (req, res) => {
   const { email, password } = req.body;
 
   // email or password 빈칸인 경우
-  if (!email || !password)
-    return res
-      .status(statusCode.BAD_REQUEST)
-      .send(util.fail(statusCode.BAD_REQUEST, responseMessage.BLANK_BOX));
+  if (!email || !password) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.BLANK_BOX));
 
   let client;
 
@@ -26,11 +23,7 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     // Firebase Authentication을 통해 유저 인증
-    const userFirebase = await signInWithEmailAndPassword(
-      firebaseAuth,
-      email,
-      password
-    )
+    const userFirebase = await signInWithEmailAndPassword(firebaseAuth, email, password)
       .then((user) => user)
       .catch((e) => {
         console.log(e);
@@ -40,9 +33,7 @@ module.exports = async (req, res) => {
     if (userFirebase.err) {
       // 제공된 식별자에 해당하는 기존 사용자 레코드가 없다.
       if (userFirebase.error.code === 'auth/user-not-found') {
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .json(util.fail(statusCode.BAD_REQUEST, responseMessage.BLANK_BOX));
+        return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST, responseMessage.BLANK_BOX));
 
         // email 사용자 속성에 제공된 값이 잘못되었다.
         //   } else if (userFirebase.error.code === "auth/invalid-email") {
@@ -52,20 +43,9 @@ module.exports = async (req, res) => {
 
         // 해당 식별자의 비밀번호가 맞지 않는다.
       } else if (userFirebase.error.code === 'auth/wrong-password') {
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .json(
-            util.fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW)
-          );
+        return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW));
       } else {
-        return res
-          .status(statusCode.BAD_REQUEST)
-          .json(
-            util.fail(
-              statusCode.INTERNAL_SERVER_ERROR,
-              responseMessage.INTERNAL_SERVER_ERROR
-            )
-          );
+        return res.status(statusCode.BAD_REQUEST).json(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
       }
     }
 
@@ -74,52 +54,54 @@ module.exports = async (req, res) => {
       user: { uid: idFirebase },
     } = userFirebase;
 
-    const user = await userDB.getUserByIdFirebase(client, idFirebase);
+    // 1-1. 유저 정보 가져오기
+    let user = await userDB.getUserByIdFirebase(client, idFirebase);
 
     const { accesstoken } = jwtHandlers.sign(user);
 
-    if (!user)
-      return res
-        .status(statusCode.BAD_REQUEST)
-        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+    if (!user) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
 
-    const userId = await userDB.getUserIdByIdFirebase(client, idFirebase);
+    // 1-2. 프로젝트 id 가져오기
+    const projectId = await projectDB.getProjectIdByUserId(client, user.id);
 
-    const projectId = await projectDB.getProjectIdByUserId(client, userId.id);
-    console.log(projectId);
+    // 1-3. user 객체에 projectId 를 병합
+    user = _.merge(user, { projectId });
 
-    const typeId = await userDB.getTypeIdByUserId(client, userId.id);
+    // 2-1. 유저의 타입 id 가져오기 (intger | null)
+    const typeId = await userDB.getTypeIdByUserId(client, user.id);
 
-    const type = await typeDB.getTypeByTypeId(client, typeId.typeId);
+    // 2-2. 해당 typeId의 데이터 가져오기
+    const type = await typeDB.getTypeByTypeId(client, typeId);
 
-    const data = {
-      user: {
-        ...user,
-        projectId,
-        type,
-      },
-    };
+    // 2-3. 해당 타입의 태그들 가져오기
+    const tag = await tagDB.getTagByTypeId(client, typeId);
+
+    // 3-1. 유저 포지션 id 배열 가져오가
+    const positionId = await positionDB.getPositionIdByUserId(client, user.id);
+
+    // 3-2. 해당 유저의 포지션들 가져오기
+    const position = await positionDB.getPositionByPositionId(client, positionId);
+
+    // 4-1. 유저 필드 id 배열 가져오가
+    const fieldId = await fieldDB.getFieldIdByUserId(client, user.id);
+
+    // 4-2. 해당 유저의 필드들 가져오기
+    const field = await fieldDB.getFieldByFieldId(client, fieldId);
 
     res.status(statusCode.OK).send(
       util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, {
-        data,
+        user,
+        type,
+        tag,
+        position,
+        field,
         accesstoken,
-      })
+      }),
     );
   } catch (error) {
-    functions.logger.error(
-      `[EMAIL LOGIN ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
-      `[CONTENT] email:${email} ${error}`
-    );
+    functions.logger.error(`[EMAIL LOGIN ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] email:${email} ${error}`);
 
-    res
-      .status(statusCode.INTERNAL_SERVER_ERROR)
-      .send(
-        util.fail(
-          statusCode.INTERNAL_SERVER_ERROR,
-          responseMessage.INTERNAL_SERVER_ERROR
-        )
-      );
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
   }

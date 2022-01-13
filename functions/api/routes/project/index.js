dd171dedf87df7d0176713ab9c28fa0b7c059111
dingding-21 <phj8221@naver.com>
2022-01-14 01:33:56 +0900

    [RENAME] change dummy to metadata

--- functions/api/routes/project/index.js
+++ functions/api/routes/project/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
 
-router.get('/dummy', checkUser, require('./projectDummyGET'));
+router.get('/metadata', checkUser, require('./projectMetadataGET'));
 
 module.exports = router;

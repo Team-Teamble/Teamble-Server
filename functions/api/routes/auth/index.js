5ea6aeaad17aa8c9d3a1b1323f786af58a187065
dingding-21 <phj8221@naver.com>
2022-01-14 05:19:09 +0900

    [FEAT] user auto login

--- functions/api/routes/auth/index.js
+++ functions/api/routes/auth/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.post('/signup', require('./authSignupPOST'));
 router.post('/login', require('./authLoginPOST'));
+router.get('/login', require('./authLoginGET'));
 
 module.exports = router;

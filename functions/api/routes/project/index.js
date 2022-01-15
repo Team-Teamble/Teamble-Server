324bf098b36a9915908050b6fec677a5ce9aa049
dingding-21 <phj8221@naver.com>
2022-01-16 03:04:56 +0900

    [FEAT] create project

--- functions/api/routes/project/index.js
+++ functions/api/routes/project/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
 
 router.get('/metadata', checkUser, require('./projectMetadataGET'));
+router.post('/', require('./projectPOST'));
 
 module.exports = router;

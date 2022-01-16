cbf4c5a1cb72477a14abe0f22e2514ca918f04c4
dingding-21 <phj8221@naver.com>
2022-01-16 10:51:13 +0900

    [FEAT] add project member

---
+++ functions/api/routes/project/projectMemberPOST.js
@@ -0,0 +1,37 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { periodDB, positionDB, goalDB, typeDB, fieldDB, userDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.PROJECT_EMAIL_BLANK_BOX));
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+    const user = await userDB.getUserByEmail(client, email);
+
+    if (!user) {
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_EXIST_EMAIL));
+    }
+
+    const data = {
+      member: user,
+    };
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.ADD_PROJECT_MEMBER_SUCCESS, data));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};

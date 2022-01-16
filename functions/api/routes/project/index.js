028b92f2e76a18e1a1d122d9505a260ce3fc2510
dingding-21 <phj8221@naver.com>
2022-01-17 03:31:21 +0900

    [RENAME] projectSearchMetadataGET.js

--- functions/api/routes/project/index.js
+++ functions/api/routes/project/index.js
@@ -5,6 +5,7 @@ const { checkUser } = require('../../../middlewares/auth');
 router.get('/metadata', checkUser, require('./projectMetadataGET'));
 router.post('/', require('./projectPOST'));
 router.post('/member', checkUser, require('./projectMemberPOST'));
+router.delete('/:projectId', checkUser, require('./projectDELETE'));
 
-router.get('/search/metadata', require('./projectSearchMetadata'));
+router.get('/search/metadata', require('./projectSearchMetadataGET'));
 module.exports = router;

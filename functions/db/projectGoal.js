2788232e7df292577d8dd9af26b9436e49895764
dingding-21 <phj8221@naver.com>
2022-01-17 03:26:34 +0900

    [ADD] db files

--- functions/db/projectGoal.js
+++ functions/db/projectGoal.js
@@ -14,4 +14,20 @@ const addGoalProject = async (client, projectId, goalId) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
-module.exports = { addGoalProject };
+const getGoalByProjectId = async (client, projectId) => {
+  const { rows } = await client.query(
+    `
+    SELECT pg.goal_id AS id, g.name AS name 
+    FROM "project" pj
+    INNER JOIN "project_goal" pg
+      ON pj.id = pg.project_id
+    INNER JOIN "goal" g
+      ON pg.goal_id = g.id
+    WHERE pj.id = $1
+    `,
+    [projectId],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+module.exports = { addGoalProject, getGoalByProjectId };

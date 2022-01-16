6e36e6d686148fae5bf1fdbcc8644cc22b3d7c79
dingding-21 <phj8221@naver.com>
2022-01-16 11:26:04 +0900

    [FEAT] get_project_search_metadata

--- functions/api/routes/project/index.js
+++ functions/api/routes/project/index.js
@@ -5,4 +5,5 @@ const { checkUser } = require('../../../middlewares/auth');
 router.get('/metadata', checkUser, require('./projectMetadataGET'));
 router.post('/', require('./projectPOST'));
 
+router.get('/search/metadata', require('./projectSearchMetadata'));
 module.exports = router;

f52650a21bc15cc349d0d1a651f717157c8164dc
dingding-21 <phj8221@naver.com>
2022-01-17 12:28:35 +0900

    [FEAT] update project photo

--- functions/api/routes/project/index.js
+++ functions/api/routes/project/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { checkUser } = require('../../../middlewares/auth');
+const uploadImage = require('../../../middlewares/uploadImage');
 
 router.get('/metadata', checkUser, require('./projectMetadataGET'));
+router.get('/search/metadata', require('./projectSearchMetadataGET'));
+
 router.post('/', require('./projectPOST'));
 router.post('/member', checkUser, require('./projectMemberPOST'));
 router.delete('/:projectId', checkUser, require('./projectDELETE'));
-
-router.get('/search/metadata', require('./projectSearchMetadataGET'));
+router.post('/photo/:projectId', checkUser, uploadImage, require('./projectPhotoPOST'));
 module.exports = router;
---
+++ functions/api/routes/project/projectPhotoPOST.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const util = require('../../../lib/util');
+const statusCode = require('../../../constants/statusCode');
+const responseMessage = require('../../../constants/responseMessage');
+const db = require('../../../db/db');
+const { projectDB } = require('../../../db');
+
+module.exports = async (req, res) => {
+  const { projectId } = req.params;
+  const imageUrls = req.imageUrls; // uploadImage 미들웨어에서 next()를 통해 넘어온 req.imageUrls를 사용
+
+  //projectId 확인
+  if (!projectId) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+    // 1. projectDB 테이블의 photo url 업데이트 하기
+    const { photo: projectPhoto } = await projectDB.updateProjectPhoto(client, projectId, imageUrls[0]);
+
+    if (!projectPhoto) return res.status(statusCode.NOT_FOUND).Pnd(util.fail(statusCode.NOT_FOUND, responseMessage.NULL_VALUE));
+
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.UPDATE_PROJECT_PHOTO_SUCCESS, { projectPhoto }));
+  } catch (error) {
+    functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
+    console.log(error);
+
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+    client.release();
+  }
+};

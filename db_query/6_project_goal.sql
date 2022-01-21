8186884cf576ed183ead0d3cefc53a52e0b77493
MoonGyu1 <s201801402@hufs-gsuite.kr>
2022-01-21 15:30:09 +0900

    [ADD] db demo queries

---
+++ db_query/6_project_goal.sql
@@ -0,0 +1,32 @@
+ALTER SEQUENCE project_goal_project_goal_id_seq RESTART WITH 1;
+
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (1, 1, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (2, 2, 3);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (3, 3, 3);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (4, 4, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (5, 5, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (6, 6, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (7, 7, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (8, 8, 3);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (9, 9, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (10, 10, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (11, 11, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (12, 12, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (13, 13, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (14, 14, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (15, 15, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (16, 16, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (17, 17, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (18, 18, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (19, 19, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (20, 20, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (21, 21, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (22, 22, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (23, 23, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (24, 24, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (25, 25, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (26, 26, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (27, 27, 4);
+INSERT INTO public.project_goal (id, project_id, goal_id) VALUES (28, 28, 4);
+
+ALTER SEQUENCE project_goal_project_goal_id_seq RESTART WITH 29;

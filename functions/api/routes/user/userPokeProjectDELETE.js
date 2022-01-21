fa9468fbf0959ff503e7357f9653ceac549d5e35
dingding-21 <phj8221@naver.com>
2022-01-21 14:08:44 +0900

    [CHORE] user slack message

--- functions/api/routes/user/userPokeProjectDELETE.js
+++ functions/api/routes/user/userPokeProjectDELETE.js
@@ -5,6 +5,7 @@ const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const { userDB, projectPokeDB } = require('../../../db');
+const slackAPI = require('../../../middlewares/slackAPI');
 
 module.exports = async (req, res) => {
   const { projectId, pokingUserId } = req.params;
@@ -29,6 +30,9 @@ module.exports = async (req, res) => {
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
+
+    const slackMessage = `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl} [CONTENT] projectId: ${projectId}, pokingUserId: ${pokingUserId} / ${error}`;
+    slackAPI.sendMessageToSlack(slackMessage, slackAPI.DEV_WEB_HOOK_ERROR_MONITORING);
 
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
